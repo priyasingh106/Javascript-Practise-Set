2ba3c6d8a465db3c59f12481ba5a6aa7bb2b500f
priyasingh106 <priyasinghrbl873.com>
2023-06-29 19:29:07 +0530

    solved

--- Question08/index.js
+++ Question08/index.js
@@ -1,2 +1,15 @@
 // print fibonacci series
 
+
+
+function fibonacci(num1, num2){
+    for(let i = 0; i < 10; i++) {
+        let storedValue = num1 + num2;
+        console.log(storedValue);
+        num1 = num2;
+        num2 = storedValue;
+    }
+}
+
+
+fibonacci(0,1);
